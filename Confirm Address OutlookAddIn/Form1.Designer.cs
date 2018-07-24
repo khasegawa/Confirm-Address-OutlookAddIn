618cdda7e4e7f7fde83d12008ce5718e32565ff0
K H <khasegawa@users.noreply.github.com>
2018-07-24 15:31:54 +0900

    Add project files.

---
+++ Confirm Address OutlookAddIn/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Confirm_Address_OutlookAddIn
+{
+    partial class FormConfirmAddress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.listViewInnerAddress = new System.Windows.Forms.ListView();
+            this.listViewOuterAddress = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonSend.Location = new System.Drawing.Point(310, 319);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(410, 319);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // listViewInnerAddress
+            // 
+            this.listViewInnerAddress.CheckBoxes = true;
+            this.listViewInnerAddress.Location = new System.Drawing.Point(12, 12);
+            this.listViewInnerAddress.Name = "listViewInnerAddress";
+            this.listViewInnerAddress.Size = new System.Drawing.Size(121, 97);
+            this.listViewInnerAddress.TabIndex = 2;
+            this.listViewInnerAddress.UseCompatibleStateImageBehavior = false;
+            this.listViewInnerAddress.View = System.Windows.Forms.View.Details;
+            this.listViewInnerAddress.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ListView_ColumnClick);
+            this.listViewInnerAddress.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.ListView_ItemChecked);
+            this.listViewInnerAddress.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListView_ItemSelectionChanged);
+            // 
+            // listViewOuterAddress
+            // 
+            this.listViewOuterAddress.CheckBoxes = true;
+            this.listViewOuterAddress.Location = new System.Drawing.Point(12, 133);
+            this.listViewOuterAddress.Name = "listViewOuterAddress";
+            this.listViewOuterAddress.Size = new System.Drawing.Size(121, 97);
+            this.listViewOuterAddress.TabIndex = 3;
+            this.listViewOuterAddress.UseCompatibleStateImageBehavior = false;
+            this.listViewOuterAddress.View = System.Windows.Forms.View.Details;
+            this.listViewOuterAddress.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ListView_ColumnClick);
+            this.listViewOuterAddress.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.ListView_ItemChecked);
+            this.listViewOuterAddress.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListView_ItemSelectionChanged);
+            // 
+            // FormConfirmAddress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 354);
+            this.ControlBox = false;
+            this.Controls.Add(this.listViewOuterAddress);
+            this.Controls.Add(this.listViewInnerAddress);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "FormConfirmAddress";
+            this.Text = "Confirm Address";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormConfirmAddress_Load);
+            this.Resize += new System.EventHandler(this.FormConfirmAddress_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ListView listViewInnerAddress;
+        private System.Windows.Forms.ListView listViewOuterAddress;
+    }
+}
---
+++ Confirm Address OutlookAddIn/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Outlook = Microsoft.Office.Interop.Outlook;
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Confirm_Address_OutlookAddIn {
+    public partial class FormConfirmAddress : Form {
+        private readonly int MARGIN = 10;
+        private Outlook.MailItem mail;
+
+        public FormConfirmAddress(Outlook.MailItem mail) {
+            this.mail = mail;
+            InitializeComponent();
+        }
+
+        private void FormConfirmAddress_Load(object sender, EventArgs e) {
+            listViewInnerAddress.Columns.Add("Inner", listViewInnerAddress.Width, HorizontalAlignment.Left);
+            listViewOuterAddress.Columns.Add("Outer", listViewInnerAddress.Width, HorizontalAlignment.Left);
+
+            RecipType[] recipTypes = new RecipType[] {
+                new RecipType(Outlook.OlMailRecipientType.olTo, "To:"),
+                new RecipType(Outlook.OlMailRecipientType.olCC, "CC:"),
+                new RecipType(Outlook.OlMailRecipientType.olBCC, "BCC:"),
+            };
+
+            string myDomain;
+            try {
+                myDomain = mail.Session.CurrentUser.AddressEntry.GetExchangeUser().PrimarySmtpAddress.Split('@')[1];
+            } catch (Exception) {
+                myDomain = ".";
+            }
+            foreach (RecipType recipType in recipTypes) {
+                foreach (Outlook.Recipient recip in mail.Recipients) {
+                    if ((Outlook.OlMailRecipientType)recip.Type == recipType.type) {
+                        var item = new ListViewItem();
+                        item.Font = new Font(item.Font, item.Font.Style | FontStyle.Bold);
+                        string smtpAddress;
+                        try {
+                            smtpAddress = recip.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x39FE001E").ToString().Trim();
+                        } catch (Exception) {
+                            smtpAddress = "?";
+                        }
+                        item.Text = recipType.text + "  " +
+                            (recip.Name.Trim().Equals(smtpAddress) ? smtpAddress : recip.Name.Trim() + " <" + smtpAddress + ">");
+                        if (smtpAddress.EndsWith("@" + myDomain) || smtpAddress.EndsWith("." + myDomain)) {
+                            listViewInnerAddress.Items.Add(item);
+                        } else {
+                            listViewOuterAddress.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            listViewInnerAddress.Left = listViewOuterAddress.Left = MARGIN;
+            listViewInnerAddress.Top = MARGIN;
+            buttonSend.Enabled = false;
+            AlignParts();
+        }
+
+        private void FormConfirmAddress_Resize(object sender, EventArgs e) {
+            AlignParts();
+        }
+
+        private void AlignParts() {
+            listViewInnerAddress.Width = listViewOuterAddress.Width = ClientRectangle.Width - MARGIN * 2;
+            listViewInnerAddress.Columns[0].Width = listViewInnerAddress.ClientRectangle.Width;
+            listViewOuterAddress.Columns[0].Width = listViewOuterAddress.ClientRectangle.Width;
+
+            listViewInnerAddress.Height = listViewOuterAddress.Height = (ClientRectangle.Height - buttonSend.Height - MARGIN * 2) / 2 - MARGIN;
+            listViewOuterAddress.Top = listViewInnerAddress.Bottom + MARGIN;
+
+            buttonCancel.Left = ClientRectangle.Width - buttonCancel.Width - MARGIN;
+            buttonSend.Left = buttonCancel.Left - buttonSend.Width - MARGIN;
+
+            buttonCancel.Top = buttonSend.Top = ClientRectangle.Height - buttonCancel.Height - MARGIN;
+        }
+
+        private void ListView_ItemChecked(object sender, ItemCheckedEventArgs e) {
+            e.Item.Font = new Font(e.Item.Font,
+                e.Item.Checked ? e.Item.Font.Style & ~FontStyle.Bold : e.Item.Font.Style | FontStyle.Bold);
+            buttonSend.Enabled = IsFullChecked(listViewInnerAddress) & IsFullChecked(listViewOuterAddress);
+        }
+        
+        private void ListView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e) {
+            if (e.IsSelected) {
+                e.Item.Checked = !e.Item.Checked;
+                e.Item.Selected = false;
+            }
+        }
+
+        private void ListView_ColumnClick(object sender, ColumnClickEventArgs e) {
+            ListView listView = (ListView)sender;
+            bool stat = IsFullChecked(listView);
+            foreach (ListViewItem item in listView.Items) {
+                item.Checked = !stat;
+            }
+        }
+
+        private bool IsFullChecked(ListView listView) {
+            return listView.CheckedItems.Count == listView.Items.Count;
+        }
+
+        internal struct RecipType {
+            internal Outlook.OlMailRecipientType type;
+            internal string text;
+            internal RecipType(Outlook.OlMailRecipientType type, string text) {
+                this.type = type;
+                this.text = text;
+            }
+        }
+    }
+}
